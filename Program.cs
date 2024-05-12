9dc103af39805c05d23cf8c3e9a04a01d8373a40
Mari Ryazantseva <marirya95@yandex.ru>
2024-05-12 12:32:47 +0300

    Добавлена программа для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountStrings(string[] array, int maxLength)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            size ++;
+        }
+    }
+    return size;
+}
+
+
+string[] CreateNewArrayStrMax(string[] array, int maxLength)
+{
+    int z = CountStrings(array, maxLength);
+    string[] NewArray = new string[z];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            NewArray[j++] = array[i];
+        }
+    }
+    return NewArray;
+}
+
+
+// string[] array = new string[] { "Hello", "2", "world", ":-)" };
+
+string[] array = new string[] { "1234", "1567", "-2" };
+
+// string[] array = new string[] { "Russia", "Denmark", "Kazan" };
+
+int maxLength = 3;
+
+string[] newArray = CreateNewArrayStrMax(array, maxLength);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(newArray);
